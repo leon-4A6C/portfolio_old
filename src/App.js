5ccb5163d048aa2a90bfdfe354d663b61118bbc8
leon in 't Veld <leon3110l@gmail.com>
2017-09-30 14:56:53 +0200

    added the basics

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { light } from './themes'
+import { Home } from './views'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ThemeProvider theme={light}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:page" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
