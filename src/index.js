080408a4c1a83188beb1287723594677b093881d
leon in 't Veld <leon3110l@gmail.com>
2017-10-07 22:28:00 +0200

    added react-apollo

--- src/index.js
+++ src/index.js
@@ -2,23 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import {
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider,
+} from 'react-apollo'
 
 import registerServiceWorker from './registerServiceWorker'
 import { light } from './themes'
 import Main from './views'
 import './index.css'
+import { github } from './token'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.github.com/graphql',
+})
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {} // Create the header object if needed.
+      }
+
+      // Send the login token in the Authorization header
+      req.options.headers.authorization = `Bearer ${github}`
+      next()
+    },
+  },
+])
+
+const apollo = new ApolloClient({
+  networkInterface,
+})
 
 class App extends React.Component {
   render() {
     return (
-      <ThemeProvider theme={light}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/:page" component={Main} />
-          </Switch>
-        </BrowserRouter>
-      </ThemeProvider>
+      <ApolloProvider client={apollo}>
+        <ThemeProvider theme={light}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/:page" component={Main} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ApolloProvider>
     )
   }
 }
--- src/views/projects.js
+++ src/views/projects.js
@@ -1,19 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
+import { gql, graphql } from 'react-apollo'
 
 import { Wrapper, GoDown } from '../components'
 
 const H2 = styled.h2`text-align: center;`
 
+const ProjectsWrapper = styled.div``
+
 class Projects extends React.Component {
   render() {
+    let repos = []
+    if (!this.props.data.loading) {
+      repos = this.props.data.user.repositories.nodes
+    }
     return (
       <Wrapper id="projects">
         <GoDown top to="aboutMe" />
         <H2>some of my awesome projects</H2>
+        <ProjectsWrapper>
+          {repos
+            .filter(x => !x.isFork)
+            .slice(0, 6)
+            .map((x, i) => <div key={i}>{x.name}</div>)}
+        </ProjectsWrapper>
       </Wrapper>
     )
   }
 }
 
-export default Projects
+export default graphql(gql`
+  query {
+    user(login: "leon-4A6C") {
+      repositories(first: 7, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        nodes {
+          name
+          description
+          isFork
+          license
+          updatedAt
+          createdAt
+          mentionableUsers(first: 10) {
+            nodes {
+              login
+              name
+            }
+          }
+          languages(first: 3, orderBy: { field: SIZE, direction: DESC }) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`)(Projects)
