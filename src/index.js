ef6ab25cdbdaf7d25077712d6316d8bb9230acf9
leon in 't Veld <leon3110l@gmail.com>
2017-10-07 21:54:13 +0200

    cleaned up some code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
 import registerServiceWorker from './registerServiceWorker'
+import { light } from './themes'
+import Main from './views'
+import './index.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <ThemeProvider theme={light}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/:page" component={Main} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
