e83b7437ee48feaf0b63a0037c0c72885e53e84c
leon in 't Veld <leon3110l@gmail.com>
2017-10-21 23:58:41 +0200

    added some comments

--- src/components/project.js
+++ src/components/project.js
@@ -12,6 +12,13 @@ const Wrapper = styled.div`
 const H2 = styled.h2``
 const P = styled.p``
 
+/* STUFF to add
+ *
+ * When clicked swhoop in with a more detailed list
+ * When it enters screen do some funky animation
+ * Randomly have animations across all the projects to draw viewers attention
+ *
+ */
 export default class Project extends React.Component {
   render() {
     const { name, description } = this.props.data
