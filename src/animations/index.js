6736d20350ac5ba6c35ca43d92d33785575d2e05
leon in 't Veld <leon3110l@gmail.com>
2017-10-22 21:49:29 +0200

    added some animations to the projects

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,5 +1,18 @@
 import { flyIn, flyInRev, flyOut, flyOutRev } from './fly'
 import { fadeIn, fadeOut } from './fade'
 import { floatY, floatX } from './float'
+import { popIn } from './pop'
+import { pressIn } from './press'
 
-export { flyIn, flyInRev, flyOut, flyOutRev, fadeIn, fadeOut, floatY, floatX }
+export {
+  flyIn,
+  flyInRev,
+  flyOut,
+  flyOutRev,
+  fadeIn,
+  fadeOut,
+  floatY,
+  floatX,
+  popIn,
+  pressIn,
+}
--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import Waypoint from 'react-waypoint'
+
+import { popIn, pressIn } from '../animations'
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+`
 
 const Wrapper = styled.div`
   overflow: hidden;
   padding: 1em;
   border: ${({ theme }) => theme.colors.primaryText} solid 2px;
   margin: 1em;
+  animation: ${({ open }) => (open ? pressIn : popIn)}
+    ${({ open }) => (open ? '300ms' : '1s')} linear;
+  width: 100%;
   flex: 1;
 `
 
@@ -20,13 +31,32 @@ const P = styled.p``
  *
  */
 export default class Project extends React.Component {
+  state = {
+    inView: false,
+    open: false,
+  }
+
   render() {
     const { name, description } = this.props.data
     return (
-      <Wrapper {...this.props}>
-        <H2>{name}</H2>
-        <P>{description}</P>
-      </Wrapper>
+      <Container>
+        <Waypoint
+          onEnter={() => {
+            setTimeout(() => this.setState({ inView: true }), 1000)
+            console.log('onEnter')
+          }}
+        />
+        {this.state.inView && (
+          <Wrapper
+            {...this.props}
+            {...this.state}
+            onClick={() => this.setState({ open: true })}
+          >
+            <H2>{name}</H2>
+            <P>{description}</P>
+          </Wrapper>
+        )}
+      </Container>
     )
   }
 }
